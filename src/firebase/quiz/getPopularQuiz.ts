d9b959632e06cfa9be44def0ebf586c1a98c4da4
Muh. Wira <muhwira907@gmail.com>
2024-01-16 21:20:25 +0800

    feat: implement function to fetch popular quizzes from firebase
    - Implemented 'getPopularQuiz' function to fetch popular quizzes from Firebase based on the number of players.
    - Translated quiz data based on the specified language ('en' or 'id').
    
    This allows retrieving popular quizzes for display in the application.

---
+++ src/firebase/quiz/getPopularQuiz.ts
@@ -0,0 +1,39 @@
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore';
+import { db } from '../config';
+import { QuizProps } from './quiz';
+
+export async function getPopularQuiz(
+  lang: string
+): Promise<QuizProps[] | undefined> {
+  try {
+    const quizzesQuery = query(
+      collection(db, 'quiz'),
+      orderBy('numberOfPlayers', 'desc'),
+      limit(3)
+    );
+    const quizzesSnapshot = await getDocs(quizzesQuery);
+
+    const quizzes: QuizProps[] = [];
+    quizzesSnapshot.forEach((quizDocSnap) => {
+      const quizData = quizDocSnap.data();
+      const quiz = {
+        id: quizDocSnap.id,
+        name: lang === 'en' ? quizData.name.en : quizData.name.id,
+        category: lang === 'en' ? quizData.category.en : quizData.category.id,
+        thumbnail: quizData.thumbnail,
+        numberOfPlayers: quizData.numberOfPlayers,
+        numberOfQuestions: quizData.numberOfQuestions,
+        timePerQuestion: quizData.timePerQuestion,
+        scorePerQuestion: quizData.scorePerQuestion,
+        questions:
+          lang === 'en' ? quizData.questions.en : quizData.questions.id,
+      };
+      quizzes.push(quiz);
+    });
+
+    return quizzes;
+  } catch (error) {
+    console.error('Error getting popular quiz:', error);
+    return undefined;
+  }
+}
