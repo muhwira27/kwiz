d328eee9c8965bf60f240285c97a5cb455838b1e
Muh. Wira <muhwira907@gmail.com>
2024-01-21 10:41:45 +0800

    feat: create dynamic RootLayout for localized pages
    Implemented a RootLayout component to provide a consistent layout across different localized pages. This layout includes:
    - A Sidebar component that adapts to the navigation menu based on the current language.
    - A Header component that contains search and user account functionality.

---
+++ src/app/[lang]/(pages)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '@/lib/dictionary';
+import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
+import { SidebarProvider } from '@/context/SidebarContext';
+
+export const metadata: Metadata = {
+  title: 'Kwiz',
+  description:
+    'Kwiz: Your interactive platform with various quiz categories. Learn, play and track your progress!',
+};
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { lang: Locale };
+}) {
+  const { menu } = await getDictionary(params.lang);
+  const { header } = await getDictionary(params.lang);
+
+  return (
+    <SidebarProvider>
+      <section className="flex h-screen w-screen">
+        <Sidebar menu={menu} lang={params.lang} />
+        <section className="flex w-full flex-col">
+          <Header header={header} />
+          {children}
+        </section>
+      </section>
+    </SidebarProvider>
+  );
+}
