3254167f9a2e3a88ee3ae2999cc0191610b72f9f
Muh. Wira <muhwira907@gmail.com>
2024-01-30 12:54:43 +0800

    feat: 5 - add info badge component

---
+++ src/components/InfoBadge/index.tsx
@@ -0,0 +1,32 @@
+import { SvgIconTypeMap } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
+type InfoBdgeProps = {
+  icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string;
+  };
+  label: string;
+  value: string;
+};
+
+export default function InfoBadge(props: InfoBdgeProps) {
+  return (
+    <div className="flex w-[155px] items-center gap-3 bg-transparent sm:w-40 md:w-[180px] lg:w-48 lg:gap-4">
+      <div className="flex items-center justify-center rounded-xl bg-white p-3 shadow-custom1">
+        <props.icon
+          className="text-slate-grey"
+          sx={{ fontSize: { xs: 24, sm: 26, md: 28, lg: 30 } }}
+        ></props.icon>
+      </div>
+
+      <div className="flex flex-col items-start">
+        <p className="text-base font-bold text-slate-grey md:text-lg">
+          {props.value}
+        </p>
+        <p className="text-slate-gre text-xs text-slate-grey md:text-sm">
+          {props.label}
+        </p>
+      </div>
+    </div>
+  );
+}
