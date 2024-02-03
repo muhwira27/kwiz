b8838dde38dc4525d73fe60bcd4eb872ce96f9fe
Muh. Wira <muhwira907@gmail.com>
2024-02-03 17:07:20 +0800

    feat: add function to get quiz by id

---
+++ src/firebase/quiz/getQuizById.ts
@@ -0,0 +1,27 @@
+import { db } from '../config';
+import { doc, getDoc } from 'firebase/firestore';
+import { QuizProps } from './quiz';
+
+export const getQuizById = async (quizId: string, lang: string): Promise<QuizProps | undefined>  => {
+  try {
+    const quizRef = doc(db, 'quiz', quizId);
+    const quizDoc = await getDoc(quizRef);
+    const quizData = quizDoc.data();
+    const quiz = {
+      id: quizDoc.id,
+      name: lang === 'en' ? quizData?.name.en : quizData?.name.id,
+      category: lang === 'en' ? quizData?.category.en : quizData?.category.id,
+      thumbnail: quizData?.thumbnail,
+      numberOfPlayers: quizData?.numberOfPlayers,
+      numberOfQuestions: quizData?.numberOfQuestions,
+      timePerQuestion: quizData?.timePerQuestion,
+      scorePerQuestion: quizData?.scorePerQuestion,
+      questions:
+          lang === 'en' ? quizData?.questions.en : quizData?.questions.id,
+    };
+    return quiz
+  } catch (error) {
+    console.error('Error quiz by id:', error);
+    throw error;
+  }
+};
