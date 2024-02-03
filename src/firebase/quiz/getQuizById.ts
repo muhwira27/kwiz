ba5e414fe8ecff0632e192017e3ff4929bb0737e
Muh. Wira <muhwira907@gmail.com>
2024-02-03 21:11:38 +0800

    refactor: refactor function to handle non-existent quiz document

--- src/firebase/quiz/getQuizById.ts
+++ src/firebase/quiz/getQuizById.ts
@@ -6,22 +6,28 @@ export const getQuizById = async (quizId: string, lang: string): Promise<QuizPro
   try {
     const quizRef = doc(db, 'quiz', quizId);
     const quizDoc = await getDoc(quizRef);
-    const quizData = quizDoc.data();
-    const quiz = {
-      id: quizDoc.id,
-      name: lang === 'en' ? quizData?.name.en : quizData?.name.id,
-      category: lang === 'en' ? quizData?.category.en : quizData?.category.id,
-      thumbnail: quizData?.thumbnail,
-      numberOfPlayers: quizData?.numberOfPlayers,
-      numberOfQuestions: quizData?.numberOfQuestions,
-      timePerQuestion: quizData?.timePerQuestion,
-      scorePerQuestion: quizData?.scorePerQuestion,
-      questions:
-          lang === 'en' ? quizData?.questions.en : quizData?.questions.id,
-    };
-    return quiz
+    
+    if(quizDoc.exists()) {
+      const quizData = quizDoc.data();
+      const quiz = {
+        id: quizDoc.id,
+        name: lang === 'en' ? quizData?.name.en : quizData?.name.id,
+        category: lang === 'en' ? quizData?.category.en : quizData?.category.id,
+        thumbnail: quizData?.thumbnail,
+        numberOfPlayers: quizData?.numberOfPlayers,
+        numberOfQuestions: quizData?.numberOfQuestions,
+        timePerQuestion: quizData?.timePerQuestion,
+        scorePerQuestion: quizData?.scorePerQuestion,
+        questions:
+            lang === 'en' ? quizData?.questions.en : quizData?.questions.id,
+      };
+      return quiz;
+    }
+    
+    return undefined;
+
   } catch (error) {
     console.error('Error quiz by id:', error);
-    throw error;
+    return undefined;
   }
 };
