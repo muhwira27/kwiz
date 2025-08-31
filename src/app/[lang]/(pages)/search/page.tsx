36b17ea7c6701f1680ff995214a6d4dca158854f
Muh. Wira <muhwira907@gmail.com>
2025-08-31 15:42:49 +0800

    fix: Update search results text to use HTML entities for proper quotation marks

--- src/app/[lang]/(pages)/search/page.tsx
+++ src/app/[lang]/(pages)/search/page.tsx
@@ -17,7 +17,7 @@ export default async function SearchPage({
     <main className="mb-3 mt-2 flex h-fit w-full flex-col gap-10 rounded-large bg-white px-6 py-6 shadow-custom1 sm:mt-3 md:mb-5 lg:px-9 lg:py-10">
       <section className="flex w-lvw flex-col gap-2 text-slate-grey md:w-full">
         <h2 className="text-lgx font-bold sm:text-xxl">{search.title}</h2>
-        <p className="text-base sm:text-lgx">{search.resultsFor} "{q}"</p>
+        <p className="text-base sm:text-lgx">{search.resultsFor} &ldquo;{q}&rdquo;</p>
       </section>
       <SearchClient lang={params.lang} query={q} quizCard={quizCard} />
     </main>
