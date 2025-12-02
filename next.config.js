64ddc2d39400af73a7bb7b4a3192f4317899830c
muhwira27 <muhwira907@gmail.com>
2025-12-02 19:52:06 +0800

    feat: Add image optimization settings and rewrite rules for thumbnail paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:lang/thumbnails/:path*',
+        destination: '/thumbnails/:path*',
+      },
+    ];
   },
 };
 
